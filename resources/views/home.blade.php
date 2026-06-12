13fc9a901f20edf522f03d5cfc039150cfad40de
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-12 12:23:54 +0545

    feat:add home.blade.php file

---
+++ resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+<!-- means use app.blade.php as parent template  -->
+@section('content')
+<h2>Welcome Home</h2>
+<b>This content is showed using ->yield('content')'</b>
+
+@endsection
