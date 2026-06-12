b44cc61d0d3bd72a2777666262b0bcb9da9b47de
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-12 16:46:30 +0545

    docs: add comments for better code understanding

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -1,8 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+// namespace defines where this file belongs in laravel strcture
+// App/
+// |_Http/
+//     |_Controllers/<- this file is here which is mentioned
 
+// import request class
 use Illuminate\Http\Request;
+// illuminate is the engine inside the app,core laravel library
 
 class PageController extends Controller
 {
