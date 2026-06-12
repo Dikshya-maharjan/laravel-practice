5a6a16baf4df0daacda9c2aaf58f58e3b51b4ac4
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-12 13:02:53 +0545

    feat:add css style for UI enhancement

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    
+    <!-- added css style using asset-->
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
 </head>
 <body>
     <h1>Header</h1>
@@ -12,6 +13,7 @@
         <li>Home</li>
         <li>About us</li>
         <li>Contact us</li>
+</ul>
     @yield('content')
     <footer>
         Copyright 2026
