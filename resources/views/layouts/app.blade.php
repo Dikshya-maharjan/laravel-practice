1e0850d31696a28f9fa1e87389e8f95a71b993c1
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-12 12:18:58 +0545

    feat:add app.blade.php file

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+</head>
+<body>
+    <h1>Header</h1>
+    <ul>
+        <li>Home</li>
+        <li>About us</li>
+        <li>Contact us</li>
+    @yield('content')
+    <footer>
+        Copyright 2026
+</footer>
+</body>
+</html>
