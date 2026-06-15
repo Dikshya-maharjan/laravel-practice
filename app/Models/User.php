07a221193d34d9765c05d8b2e3c35ef10d0dcc75
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-15 14:30:58 +0545

    feat:update User.php file

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,14 +1,16 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Users;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Model
+
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
