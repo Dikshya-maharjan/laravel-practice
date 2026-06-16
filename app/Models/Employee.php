11e5ee7db7dee6a8b9b42849df466af3cccf51dd
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-06-16 15:47:15 +0545

    feat: add model file Employee.php

---
+++ app/Models/Employee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable=['name','age','email','role_id'];
+    public function role(){
+        {
+            return $this->belongsTo(Role::class);//many emp can have same role but one emp has only 1 role
+            //each employee is linked to one role using key 
+            
+        }
+        
+    }
+
+}
