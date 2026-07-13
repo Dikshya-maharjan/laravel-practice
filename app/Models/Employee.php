07453be220fa8f06da9141fb6833130cd6dea0f6
Dikshya Maharjan <dikshyamahrzan@gmail.com>
2026-07-13 20:59:16 +0545

    feat:add employee models file

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -9,7 +9,7 @@ class Employee extends Model
     protected $fillable=['name','age','email','role_id'];
     public function role(){
         {
-            return $this->belongsTo(Role::class);//many emp can have same role but one emp has only 1 role
+            return $this->belongsToMany(Role::class);//many emp can have same role but one emp has only 1 role
             //each employee is linked to one role using key 
             
         }
